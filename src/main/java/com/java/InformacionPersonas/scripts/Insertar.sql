b87634a6fe2e07ecf385d21f9e87cb69643680f2
USUARIO <USUARIO@DESKTOP-UCSMMLV>
2022-12-14 13:12:12 +0100

    nuevo commit

---
+++ src/main/java/com/java/InformacionPersonas/scripts/Insertar.sql
@@ -0,0 +1,8 @@
+-- INSERTAR Tabla Persona 
+use pruebapersonas3;
+
+INSERT INTO persona(Nombre, Apellido1, Apellido2, Edad) VALUES
+('Elena','Perez','Ramos', 22),
+('Marisa','Hernandez','Lopez', 68),
+('Juan','Garcia','Conde', 41),
+('Pepe','Santos','Estebanez', 78);
